edf72de94b52c0932145ddb34125f80ee464ab96
arteemm <arteemmarteemm@gmail.com>
2021-03-15 22:24:26 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    
+    let bracketsOpen = ['(', '{', '[', '|'];
+    let bracketsClose = [')', '}', ']', '|'];
+    let current ;
+    let add;
+    let stack = [];
+  
+    for(let i = 0; i <= str.length-1; i++){
+        
+        current = str[i];
+  
+        if (bracketsClose.indexOf(current) > -1) {
+            add = bracketsOpen[bracketsClose.indexOf(current)];
+            if (stack.length == 0 || (stack.pop() != add)){
+                return false;
+            }
+        } else {
+            stack.push(current);
+        }
+  
+    }
+     return (stack.length == 0);
 }
